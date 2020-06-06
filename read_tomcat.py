5c2d6b95b22f71ae10ba1bf352fc350f492c2d6b
rlin2k1 <rlin2k1@gmail.com>
2020-06-05 22:40:39 -0700

    Adds read intensive test for Apache Tomcat server.

--- read_tomcat.py
+++ read_tomcat.py
@@ -1,34 +1,29 @@
-# locust_test.py
-# In python, '#' is used to indicate a comment line.
-"""
-The string within triple-quote is also considered as a comment.
-And the triple-quote can be used for multiline comments.
-DISCLAIMER: This sample doesn't care about whether the use of API is correct.
+""" read_tomcat.py
+Simulating the scenario where all requests from users are read intensive.
+The user whose name is cs144 would randomly open one of his posts via
+/editor/post?action=open&username=cs144&postid={num}, where {num} is a random postid.
+
+Author(s):
+    Roy Lin
+
+Date Created:
+    June 5th, 2020
 """
 
 import sys, random
 from locust import HttpLocust, TaskSet
 
-def getList(locust):
-    """ define a function in python whose name is getList and the argument is locust """
-    locust.client.get('/api/cs144')
-    locust.client.get('/editor/post?action=list&username=cs144')
-
-def previewPage(locust):
-    """ define a function in python whose name is previewPage and the argument is locust """
-    postid = random.randint(1, 100) # generate a random number from 1 to 100 (include 1 and 100)
-    url_prefix = '/blog/cs144/';
-    locust.client.get(url_prefix + str(postid), name=url_prefix)
+def readPost(locust):
+    postid = random.randint(1, 500) # generate a random number from 1 to 500 (include 1 and 500)
+    url_prefix = "/editor/post?action=open"
+    url_suffix = "&username=cs144&postid="
+    locust.client.get(url_prefix + url_suffix + str(postid), name = url_prefix)
 
 class MyTaskSet(TaskSet):
-    """ the class MyTaskSet inherits from the class TaskSet, defining the behavior of the user """
-    tasks = {getList: 2, previewPage: 1}
+    tasks = [readPost]
     def on_start(locust):
         """ on_start is called when a Locust start before any task is scheduled """
-        response = locust.client.post("/login", data={"username":"cs144", "password": "password"})
-        if response.status_code != 200:
-            print("FAIL to start with posting data to server. Make sure that your server is running.")
-            sys.exit();
+        pass
 
 class MyLocust(HttpLocust):
     """ the class MyLocust inherits from the class HttpLocust, representing an HTTP user """
